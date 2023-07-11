c27934dba80fa0c80a15e5b68d28a66ccc0c8e06
Akmal Yudistira <akmalyudistira1212@gmail.com>
2023-07-11 15:58:42 +0900

    membuat autentikasi

--- config/Database.js
+++ config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("datapresnsi", "presensi", "K0p1jand@", {
+const db = new Sequelize("datapresnsi", "root", "", {
   host: "localhost",
   dialect: "mysql",
 });
